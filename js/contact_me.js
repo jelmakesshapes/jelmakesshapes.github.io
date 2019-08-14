b5d8e3c1c9322d399222397192f4cd612844ef79
Andrew <andrew@stephenson.cloud>
2019-08-14 21:42:30 +0100

    dev container and emailing

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,7 +10,6 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -18,14 +17,15 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://1pfx58wvk2.execute-api.eu-west-2.amazonaws.com/dev/mail",
                 type: "POST",
-                data: {
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify({
                     name: name,
-                    phone: phone,
                     email: email,
                     message: message
-                },
+                }),
                 cache: false,
                 success: function() {
                     // Success message
